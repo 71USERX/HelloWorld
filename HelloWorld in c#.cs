516b148cbeffe4ab218137d7e405e937a61eebba
71USERX <68045727+71USERX@users.noreply.github.com>
2020-07-23 09:35:01 +0800

    HelloWorld in c#

---
+++ HelloWorld in c#.cs
@@ -0,0 +1,13 @@
+using system;
+
+namespace HelloWorld
+{
+  class HelloWorld
+  {
+    static void Main(String[] args)
+    {
+      Console.WriteLine("HelloWorld");
+      Console.ReadKey();
+    }
+  }
+}
